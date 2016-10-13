03bc9b229e77d58b568521e4e20399f78b679cd6
gubelli <a>
2016-10-13 14:26:07 +0200

    Create basic store function and sample data

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -1,32 +1,34 @@
-var store = require("../services/orderStore.js");
+var store = require("../services/noteStore.js");
 
 module.exports.showIndex = function(req, res)
 {
     res.render("index");
 };
 
-module.exports.createOrder = function(req, res)
+module.exports.createNote = function(req, res)
 {
-    res.render("newOrder");
+    res.render("note");
 };
 
-module.exports.createPizza = function(req, res)
+/*module.exports.createPizza = function(req, res)
 {
     var order = store.add(req.body.name, "unkown", function(err, order) {
         res.render("succeeded", order);
     });
-};
+};*/
 
-module.exports.showOrder = function(req, res)
+module.exports.editNote = function(req, res)
 {
-    store.get(req.params.id, function(err, order) {
-        res.render("showorder", order);
+    store.get(req.params.id, function(err, note) {
+        res.render("editNote", note);
     });
 };
 
+/*
 module.exports.deleteOrder =  function (req, res)
 {
     store.delete(  req.params.id , function(err, order) {
         res.render("showorder", order);
     });
 };
+*/
