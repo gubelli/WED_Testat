d5e04b3141450bfb2f918ee80b7073b4c3af788b
Luca Salzani <Alucard1766@users.noreply.github.com>
2016-10-13 14:51:48 +0200

    WED Lesson save

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('note', { title: 'Express' });
 });
 
 module.exports = router;
